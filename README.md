21bb5ebda078ded4fca1644d416b69927b290e28
gojaebeom <const.gjb@gmail.com>
2020-06-07 19:28:52 +0900

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+## chepter 01
+- index.html 파일 생성 
+- src에 com.example.web.ServletTest 클래스 생성 : HttpServlet 클래스를 상속 받아 클라이언트와 통신하기 위한 클래스
+- WEB-INF/web.xml에서 위 클래스를 매핑해줌 
+- servlet 출력도구인 HttpServletReponse 사용하여 클라이언트로 출력하기
---
+++ src/com/example/web/ServletTest.java
@@ -0,0 +1,23 @@
+package com.example.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletTest extends HttpServlet
+{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException
+	{
+		
+		//서블릿 출력도구 HttpServletResponse 사용하여 출력하기
+		PrintWriter out =  res.getWriter();
+		out.println("hello servlet");
+	}
+}
