e99d2c549559b9c1fd8924896964ec25c6d85928
gojaebeom <const.gjb@gmail.com>
2020-06-07 19:51:55 +0900

    update

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - src에 com.example.web.ServletTest 클래스 생성 : HttpServlet 클래스를 상속 받아 클라이언트와 통신하기 위한 클래스
 - WEB-INF/web.xml에서 위 클래스를 매핑해줌 
 - servlet 출력도구인 HttpServletReponse 사용하여 클라이언트로 출력하기
+
+## chepter 02
+- web.xml에서 매핑했던 servlet 파일을 어노테이션을 사용하여 매핑함
+- 조건으로 web.xml의 root element인 metadata-complete의 값을 false로 바꿔주어야 함 
+- true : servlet 관련 설정을 web.xml파일 내부에서 처리
+- false : servlet 관련 설정이 web.xml 파일 내부에 존재하지 않으면 다른 곳도 찾음
--- src/com/example/web/ServletTest.java
+++ src/com/example/web/ServletTest.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/test")
 public class ServletTest extends HttpServlet
 {
 	
@@ -15,6 +17,7 @@ public class ServletTest extends HttpServlet
 	protected void service(HttpServletRequest req, HttpServletResponse res) 
 			throws ServletException, IOException
 	{
+		
 		
 		//서블릿 출력도구 HttpServletResponse 사용하여 출력하기
 		PrintWriter out =  res.getWriter();
